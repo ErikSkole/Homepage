578bec2cca082bb838426e18ec052fb76f3de7fc
ErikSkole <e-erijoh6@iskule.no>
2024-01-25 13:28:23 +0100

    Add background color to body and lower page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <main className='mainContent flex flex-col'>
-      <div className='flex flex-col items-center justify-center p-48'>
+      <div className='mainHeaderContainer flex flex-col items-center justify-center mb-80 p-48'>
         <h1 className='mainHeader flex'>Welcome</h1>
         <p className='mainUnder flex'>This is my website</p>
       </div>
+      <div className="lowerPage flex items-center justify-center"></div>
     </main>
   )
 }
