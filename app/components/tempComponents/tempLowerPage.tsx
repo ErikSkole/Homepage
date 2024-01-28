e77506a80f235b5af597e9af98f9109e3d55f5af
ErikSkole <e-erijoh6@iskule.no>
2024-01-28 18:44:29 +0100

    Add TempLowerPage component and update global CSS

---
+++ app/components/tempComponents/tempLowerPage.tsx
@@ -0,0 +1,11 @@
+export default function TempLowerPage() {
+    return (
+        <>
+        <div className="tempContent flex flex-col justify-center items-center mb-40">
+          <h1 className="tempHeader flex mb-6">Projects</h1>
+          <div className="lineObject mb-16"></div>
+          <div className="tempImg flex"></div>
+        </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import TempLowerPage from "./components/tempComponents/tempLowerPage";
+
 export default function Home() {
   return (
     <main className='mainContent flex flex-col'>
@@ -5,8 +7,15 @@ export default function Home() {
         <h1 className='mainHeader flex'>Welcome</h1>
         <p className='mainUnder flex'>This is my website</p>
       </div>
-      <div className="lowerPage flex items-center justify-center">
+      <div className="lowerPage flex items-start justify-evenly pt-80">
         <div className="arrowLower">
+        </div>
+        <div className="rightCol flex flex-col">
+          <TempLowerPage />
+          <TempLowerPage />
+        </div>
+        <div className="leftCol flex flex-col mt-80">
+          <TempLowerPage />
         </div>
       </div>
     </main>
