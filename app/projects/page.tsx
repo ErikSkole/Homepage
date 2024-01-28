c2959ee7e5066051c5968acb51248b385a8e9194
ErikSkole <e-erijoh6@iskule.no>
2024-01-28 16:53:57 +0100

    Added a projects page with header and triangle svg

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+        <>
+            <div className="projectsHeaderContainer flex justify-center p-20">
+                <h1 className="mainHeader projectsHeader flex">Projects</h1>
+            </div>
+        </>
+    )
+}
