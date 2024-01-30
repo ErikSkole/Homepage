ceabd2916c3376bc0f8120e7355b550263c74160
ErikSkole <e-erijoh6@iskule.no>
2024-01-30 14:30:32 +0100

    da na nzay russkogo

--- app/components/tempComponents/tempLowerPage.tsx
+++ app/components/tempComponents/tempLowerPage.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function TempLowerPage() {
     return (
         <>
         <div className="tempContent flex flex-col justify-center items-center mb-40">
           <h1 className="tempHeader flex mb-6">Projects</h1>
           <div className="lineObject mb-16"></div>
-          <div className="tempImg flex"></div>
+          <Link className="tempImg flex" href="/projects"></Link>
         </div>
         </>
     )
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,9 @@ export default function Projects() {
             <div className="projectsHeaderContainer flex justify-center p-20">
                 <h1 className="mainHeader projectsHeader flex">Projects</h1>
             </div>
+            <div className="lowerProjects flex flex-col">
+
+            </div>
         </>
     )
 }
