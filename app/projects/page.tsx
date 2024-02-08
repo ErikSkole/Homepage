dbddf87910f1bd8e5e4d1d148ee0b7995d6cb036
ErikSkole <e-erijoh6@iskule.no>
2024-02-08 11:23:16 +0100

    Added scroll function for hiding navbar on the top

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,28 @@
+'use client'
 import Navbar from "../components/navbar/navbar"
+import { useEffect, useState } from "react";
+
 
 export default function Projects() {
+    const [showNavbar, setShowNavbar] = useState(false);
+
+    useEffect(() => {
+        const checkScroll = () => {
+            if (!showNavbar && window.pageYOffset > 500) {
+                setShowNavbar(true);
+            } else if (showNavbar && window.pageYOffset <= 500) {
+                setShowNavbar(false);
+            }
+        };
+
+        window.addEventListener('scroll', checkScroll);
+
+        // Clean up the component
+        return () => window.removeEventListener('scroll', checkScroll);
+    }, [showNavbar]);
     return (
         <>
-            <Navbar />
+            {showNavbar && <Navbar />}
             <div className="projectsHeaderContainer flex justify-center p-20">
                 <h1 className="mainHeader projectsHeader flex">Projects</h1>
             </div>
